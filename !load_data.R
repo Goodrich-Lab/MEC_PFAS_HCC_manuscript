7ac86c9ecb21da63b55a8e654db2845a0d342bfe
Hongxu Wang <hongxuwa@Hongxus-MacBook-Pro.local>
2024-09-05 10:49:15 -0700

    update

--- !load_data.R
+++ !load_data.R
@@ -34,8 +34,10 @@ untargeted_met_data <- readRDS(fs::path(dir_cleaned_data, "Untargeted_feature_me
 
 # variables--------------
 
-pfas_name <- colnames(data %>% select(contains("pfas")) %>% 
-                        select(contains("lg2")) %>% select(-contains("quartile")))
+pfas_name <- colnames(data %>% 
+                        dplyr::select(contains("pfas")) %>% 
+                        dplyr::select(contains("lg2")) %>% 
+                        dplyr::select(-contains("quartile")))
 
 prs_name <- c("prs_uw", "prs_uw_tertile")
 
@@ -45,32 +47,35 @@ data <- data %>%
   rename_at(.vars = str_c(pfas_name,"_","raw"),
             .funs = ~str_remove(., "_lg2"))
 
-pfas_name_raw <- colnames(data%>% select(contains("raw")))
+pfas_name_raw <- colnames(data%>% dplyr::select(contains("raw")))
 
 # Add PFAS quartiles, based on controls only-------
 pfas_quartile <- data %>%
   filter(casetype == "Control") 
 
 for(pfas in pfas_name) {
-  data_temp_control <- pfas_quartile %>% select(pfas)
+  data_temp_control <- pfas_quartile %>% dplyr::select(pfas)
   cuts <- quantile(data_temp_control[1], 
                    c(1/4,1/2,3/4), na.rm = TRUE)
-  data_temp <- data %>% select(pfas)
+  data_temp <- data %>% dplyr::select(pfas)
   data_temp[2] <- case_when(data_temp[1] <= cuts[1] ~ 1, 
                             data_temp[1] <= cuts[2] ~ 2,
                             data_temp[1] <= cuts[3] ~ 3,
                             !is.na(data_temp[1]) ~ 4) |> as.factor() 
   colnames(data_temp)[2] <- paste0(pfas, "_quartile")
-  data <- data %>% bind_cols(data_temp %>% select(contains("quartile")))
+  data <- data %>% bind_cols(data_temp %>% dplyr::select(contains("quartile")))
 }
 
 # filter out controls/cases with no pfas values
 data <- data %>% filter(!setnum %in% c("1938", "1350", "1006"))
 
 # dataset split by outcome
-sample_id1 <- data %>% filter(status == "HCC") %>% select(setnum)
-sample_id2 <- data %>% filter(casetype == "NAFLD-related HCC") %>% select(setnum)
-sample_id3 <- data %>% filter(casetype == "Other HCC") %>% select(setnum)
+sample_id1 <- data %>% filter(status == "HCC") %>% 
+  dplyr::select(setnum)
+sample_id2 <- data %>% filter(casetype == "NAFLD-related HCC") %>% 
+  dplyr::select(setnum)
+sample_id3 <- data %>% filter(casetype == "Other HCC") %>% 
+  dplyr::select(setnum)
 
 data_hcc <- data %>% filter(setnum %in% sample_id1$setnum) %>%
   mutate(status = ifelse(status == "HCC", 1, 0)) %>%
@@ -93,7 +98,7 @@ data_hcc <- data_hcc %>%
 
 
 # categorical pfas name
-pfas_name_cat <- colnames(data %>% select(contains("quartile")))
+pfas_name_cat <- colnames(data %>% dplyr::select(contains("quartile")))
 
 # covariates
 # covars <- c("bmi","alcohol_intake","smokestatus","diabetes", "q1_dp_amds_e_totscore")
